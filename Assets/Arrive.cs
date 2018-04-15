618e3263a06ba1548bc9703a37a00445626cac4b
Vlad Zat <vlad.zat@mydit.ie>
2018-04-15 15:52:32 +0100

    scene0

---
+++ Assets/Arrive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrive : SteeringBehaviour {
+
+    public Vector3 target = Vector3.zero;
+    public float slowingDistance = 15.0f;
+
+    [Range(0.0f, 1.0f)]
+    public float deceleration = 0.9f;
+
+    public GameObject targetGameObj = null;
+
+    public override Vector3 Calculate() {
+        return boid.ArriveForce(target, slowingDistance, deceleration);
+    }
+
+    public void Update() {
+        if (targetGameObj != null) {
+            target = targetGameObj.transform.position;
+        }
+    }
+}
