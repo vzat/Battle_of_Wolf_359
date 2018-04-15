207b2de14909c0f90db1616a86f3943446204bb5
Vlad Zat <vlad.zat@mydit.ie>
2018-04-15 22:41:38 +0100

    Add Video

--- Assets/FleetManager.cs
+++ Assets/FleetManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FleetManager : MonoBehaviour {
 
@@ -19,6 +20,12 @@ public class FleetManager : MonoBehaviour {
 
     List<Boid> ships = new List<Boid>();
 
+    int currentScene = 0;
+
+    List<bool> playedVideo = new List<bool>();
+
+    GameObject camera;
+
 	// Use this for initialization
 	void Start () {
 
@@ -29,6 +36,7 @@ public class FleetManager : MonoBehaviour {
         leader.AddComponent<Arrive>();
         leader.transform.parent = this.transform;
         leader.transform.position = leaderPos;
+        Object.DontDestroyOnLoad(this);
 
         int line = 1;
         int shipsPerLine = 0;
@@ -77,7 +85,6 @@ public class FleetManager : MonoBehaviour {
             GameObject ship = Instantiate<GameObject>(prefab);
             ship.transform.parent = this.transform;
             ship.transform.position = leader.transform.position + new Vector3((shipsPerLine - line) * (distCol + Random.Range(0, 5)), 0, - (line - 1) * (distLine + Random.Range(0, 5)));
-            //Debug.Log(new Vector3((line - shipsPerLine) * distCol, 0, -(line - 1) * distLine));
 
             ship.GetComponent<OffsetPursue>().leader = leader.GetComponent<Boid>();
 
@@ -93,11 +100,44 @@ public class FleetManager : MonoBehaviour {
             if (i == 0) {
                 mainCamera.target = ship;
             }
+
+            //Object.DontDestroyOnLoad(ship);
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        switch (currentScene) {
+            case 0:
+                if (Vector3.Distance(leader.transform.position, new Vector3(0, 0, 0)) < 100.0f) {
+                    playedVideo.Add(false);
+                    SceneManager.LoadScene("scene1");
+                    currentScene++;
+                }
+                break;
+            case 1:
+                if (!playedVideo[playedVideo.Count - 1]) {
+                    PlayVideo("./Assets/StreamingAssets/Locutus_of_Borg.mp4");
+                    playedVideo[playedVideo.Count - 1] = true;
+                }
+                break;
+            default:
+                break;
+        }
 	}
+
+    void PlayVideo (string videoUrl) {
+        camera = GameObject.Find("Main Camera");
+        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.playOnAwake = false;
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+        videoPlayer.url = videoUrl;
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.loopPointReached += EndReached;
+        videoPlayer.Play();
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp) {
+        Destroy(camera.GetComponent<UnityEngine.Video.VideoPlayer>());
+    }
 }
