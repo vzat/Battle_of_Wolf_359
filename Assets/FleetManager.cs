dc75963a35253433d184b9873fe22ae25f22946c
Vlad Zat <vlad.zat@mydit.ie>
2018-04-15 21:21:39 +0100

    Arrive at borg cube

--- Assets/FleetManager.cs
+++ Assets/FleetManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class FleetManager : MonoBehaviour {
 
-    public GameObject leader;
     public GameObject excelsiorPrefab;
     public GameObject nebulaPrefab;
     public GameObject mirandaPrefab;
@@ -12,7 +11,11 @@ public class FleetManager : MonoBehaviour {
     public GameObject oberthPrefab;
     public GameObject ambassadorPrefab;
 
+    GameObject leader;
     public int fleetNo = 40;
+    public float distLine = 10.0f;
+    public float distCol = 10.0f;
+    public Vector3 leaderPos = new Vector3(0, 0, -200);
 
     List<Boid> ships = new List<Boid>();
     
@@ -20,10 +23,39 @@ public class FleetManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		for (int i = 0; i < fleetNo; i++) {
+        FollowCamera mainCamera = Camera.main.GetComponent<FollowCamera>();
+
+        leader = new GameObject();
+        leader.AddComponent<Boid>();
+        leader.AddComponent<Arrive>();
+        leader.transform.parent = this.transform;
+        leader.transform.position = leaderPos;
+
+        int line = 1;
+        int shipsPerLine = 0;
+        for (int i = 0; i < fleetNo; i++) {
             GameObject prefab = ambassadorPrefab;
 
-            switch((int)Random.Range(1, 6)) {
+            // Generate random ship type
+            int shipType = (int)Random.Range(1, 6);
+
+            // Set Predefied Ships for the first line
+            switch (i) {
+                case 0:
+                    shipType = 3;
+                    break;
+                case 1:
+                    shipType = 1;
+                    break;
+                case 2:
+                    shipType = 6;
+                    break;
+                default:
+                    break;
+            }
+
+            // Get a random ship type
+            switch (shipType) {
                 case 1:
                     prefab = excelsiorPrefab;
                     break;
@@ -44,7 +76,24 @@ public class FleetManager : MonoBehaviour {
             }
 
             GameObject ship = Instantiate<GameObject>(prefab);
-            //ship.transform.position = 
+            ship.transform.parent = this.transform;
+            ship.transform.position = leader.transform.position + new Vector3((shipsPerLine - line) * (distCol + Random.Range(0, 5)), 0, - (line - 1) * (distLine + Random.Range(0, 5)));
+            //Debug.Log(new Vector3((line - shipsPerLine) * distCol, 0, -(line - 1) * distLine));
+
+            ship.GetComponent<OffsetPursue>().leader = leader.GetComponent<Boid>();
+
+            // There are line * 2 + 1 ships per line
+            if (shipsPerLine > line * 2 - 1) {
+                line++;
+                shipsPerLine = 0;
+            }
+
+            ships.Add(ship.GetComponent<Boid>());
+            shipsPerLine++;
+
+            if (i == 0) {
+                mainCamera.target = ship;
+            }
         }
 	}
 	
